6a6bdf7642a592a4492eb02f785d4a251285a170
WAYUDI RAMADAN <wahyudiramadhan16.04@gmail.com>
2026-06-21 16:37:47 +0700

    fix: force https scheme for production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Providers;
+use Illuminate\Support\Facades\URL;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -19,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (config('app.env') === 'production') {
+        URL::forceScheme('https');
+    }
     }
 }
